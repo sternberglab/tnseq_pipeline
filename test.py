3bb60d91b7255a327202542e00086ee345d73470
Chris Acree <acreechristopher@gmail.com>
2021-03-27 21:59:28 -0400

    go

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
-print("THIS IS A TEST")
 import botocore
 import boto3
+import os
 
 def main():
+	print("doing things")
+	print(os.cwd())
 	#with open('sqs-message.json', 'r') as msg:
 	s3 = boto3.client('s3')
 	s3.upload_file('./sqs-message.json', 'sternberg-sequencing-data', 'test/test.py')
