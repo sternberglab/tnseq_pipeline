9ad2854d90cf00d6692fdeae21aaaa9f633046d6
Chris Acree <acreechristopher@gmail.com>
2021-03-27 22:00:03 -0400

    ok

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import os
 
 def main():
 	print("doing things")
-	print(os.cwd())
+	print(os.getcwd())
 	#with open('sqs-message.json', 'r') as msg:
 	s3 = boto3.client('s3')
 	s3.upload_file('./sqs-message.json', 'sternberg-sequencing-data', 'test/test.py')
