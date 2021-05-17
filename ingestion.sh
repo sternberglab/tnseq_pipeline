bef8c927f2a5a890c96b8244e2a49ec86eaa91ac
Chris Acree <acreechristopher@gmail.com>
2021-05-17 16:00:33 -0400

    More expansion of variables

--- ingestion.sh
+++ ingestion.sh
@@ -6,8 +6,9 @@ git pull
 $(aws secretsmanager get-secret-value --secret-id NGS_PIPELINE_UNPROCESSED_SQS_URL |  jq -r '"export NGS_PIPELINE_UNPROCESSED_SQS_URL=" + .SecretString ')
 
 LOG_FILENAME="logs_"$(date +'%Y_%m_%d_%I_%M_%p_$RANDOM')".txt"
-touch "$LOG_FILENAME"
-echo "Starting processing samples" >> "$LOG_FILENAME"
+echo "${LOG_FILENAME}"
+touch "${LOG_FILENAME}"
+echo "Starting processing samples" >> "${LOG_FILENAME}"
 
 while [ /bin/true ]; do
 
@@ -22,7 +23,7 @@ while [ /bin/true ]; do
 
     if [ -z "${msg}" -o "${msg}" = "None" ]; then
     	echo "$(date) Processing complete. Stopping task."
-    	echo "Done processing everything, shutting down now" >> "$LOG_FILENAME"
+    	echo "Done processing everything, shutting down now" >> "${LOG_FILENAME}"
 		aws s3 cp ${LOG_FILENAME} s3://sternberg-sequencing-data/ngs_pipeline_outputs/tmp_logs/${LOG_FILENAME}
     	exit
     else
@@ -31,7 +32,7 @@ while [ /bin/true ]; do
 		echo "${sqs_message}" > ./sqs_message.json
 
 		receipt_handle=$(echo "${msg}" | cut -f2 --)
-		python3 ./wrapper.py 2>&1 | tee -a "$LOG_FILENAME"
+		python3 ./wrapper.py 2>&1 | tee -a "${LOG_FILENAME}"
 		CMD_EXIT=$?
 
 		if [ $CMD_EXIT -eq 0 ]; then
